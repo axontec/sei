f4a9beaf3ff8cbcb22dda29afcd19f64d90bb560
Alexandre Jose Ribeiro Soares <alexandre.soares@ima.sp.gov.br>
2017-05-04 10:29:26 -0300

    Retirada de gem

--- test/imasei_test.rb
+++ test/imasei_test.rb
@@ -1,162 +1,27 @@
 require 'test_helper'
 
-describe 'imasei' do
+describe 'wssei' do
 
   before do
-    Imasei.configure do |config|
-      config.wsdl = ENV['SEI_CONFIG_WSDL']
+    Wssei.configure do |config|
+      config.wsdl = "https://sei-treinamento.campinas.sp.gov.br/sei/controlador_ws.php?servico=sei"
       config.follow_redirects = true
       config.pretty_print_xml = true
-      config.sigla = ENV['SEI_CONFIG_SIGLA']
-      config.identificacao = ENV['SEI_CONFIG_IDENTIFICACAO']
+      config.sigla = 'DAC'
+      config.identificacao = 'SISDAC'
     end
   end
 
-  it 'tem um número de versão' do
-    refute_nil ::Imasei::VERSION
-  end
-
-  it 'gera um procedimento sem documento' do
-    procedimento = Imasei::Estruturas::Procedimento.new
-                    .id_tipo_procedimento('20')
-                    .especificacao('Especificação do processo')
-                    .assunto('01.01.01', 'Licitação')
-                    .interessado('leandro.telles', 'Leandro Telles')
-                    .observacao('Observação do webservice')
-                    .nivel_de_acesso('0')
-    
-    retorno_geracao_procedimento = Imasei::Servico.gerar_procedimento(
-                                      '110000949',
-                                      procedimento,
-                                      documentos = [],
-                                      procedimentos_relacionados = [],
-                                      unidades_envio = [],
-                                      manter_aberto_unidade = 'N',
-                                      enviar_email_notificacao = 'N',
-                                      data_retorno_programado = nil,
-                                      dias_retorno_programado = nil,
-                                      dias_uteis_retorno_programado = 'N')
-
-    refute_nil retorno_geracao_procedimento.id_procedimento
-  end
-
-  it 'gera um procedimento com 1 documento gerado' do
-    procedimento = Imasei::Estruturas::Procedimento.new
-                    .id_tipo_procedimento('20')
-                    .especificacao('Especificação do processo')
-                    .assunto('01.01.01', 'Licitação')
-                    .interessado('leandro.telles', 'Leandro Telles')
-                    .observacao('Observação do webservice')
-                    .nivel_de_acesso('0')
-    documentos = [
-      Imasei::Estruturas::Documento.new
-        .tipo('G')
-        .id_serie('20')
-        .descricao('Descrição do documento 1')
-        .remetente('Remetente do documento 1')
-        .observacao('Observação do documento 1')
-        .conteudo('Conteúdo do documento 1')
-    ]
-
-    retorno_geracao_procedimento = Imasei::Servico.gerar_procedimento(
-                                      '110000949',
-                                      procedimento,
-                                      documentos,
-                                      procedimentos_relacionados = [],
-                                      unidades_envio = [],
-                                      manter_aberto_unidade = 'N',
-                                      enviar_email_notificacao = 'N',
-                                      data_retorno_programado = nil,
-                                      dias_retorno_programado = nil,
-                                      dias_uteis_retorno_programado = 'N')
-
-    refute_nil retorno_geracao_procedimento.id_procedimento
-  end
-
-  it 'gera um procedimento com 2 documentos gerados' do
-    procedimento = Imasei::Estruturas::Procedimento.new
-                    .id_tipo_procedimento('20')
-                    .especificacao('Especificação do processo')
-                    .assunto('01.01.01', 'Licitação')
-                    .interessado('leandro.telles', 'Leandro Telles')
-                    .observacao('Observação do webservice')
-                    .nivel_de_acesso('0')
-    documentos = [
-      Imasei::Estruturas::Documento.new
-        .tipo('G')
-        .id_serie('20')
-        .descricao('Descrição do documento 1')
-        .remetente('Remetente do documento 1')
-        .observacao('Observação do documento 1')
-        .conteudo('Conteúdo do documento 1'),
-      Imasei::Estruturas::Documento.new
-        .tipo('G')
-        .id_serie('20')
-        .descricao('Descrição do documento 2')
-        .remetente('Remetente do documento 2')
-        .observacao('Observação do documento 2')
-        .conteudo('Conteúdo do documento 2')
-    ]
-
-    retorno_geracao_procedimento = Imasei::Servico.gerar_procedimento(
-                                      '110000949',
-                                      procedimento,
-                                      documentos,
-                                      procedimentos_relacionados = [],
-                                      unidades_envio = [],
-                                      manter_aberto_unidade = 'N',
-                                      enviar_email_notificacao = 'N',
-                                      data_retorno_programado = nil,
-                                      dias_retorno_programado = nil,
-                                      dias_uteis_retorno_programado = 'N')
-
-    refute_nil retorno_geracao_procedimento.id_procedimento
-  end
-
-  it 'gera um procedimento com 1 documento recebido' do
-    procedimento = Imasei::Estruturas::Procedimento.new
-                    .id_tipo_procedimento('20')
-                    .especificacao('Especificação do processo')
-                    .assunto('01.01.01', 'Licitação')
-                    .interessado('leandro.telles', 'Leandro Telles')
-                    .observacao('Observação do webservice')
-                    .nivel_de_acesso('0')
-    documentos = [
-      Imasei::Estruturas::Documento.new
-        .tipo('R')
-        .id_serie('20')
-        .numero('Nome na árvore')
-        .data(Date.today)
-        .remetente('Remetente do documento')
-        .nome_arquivo('ws-manual.pdf')
-        .conteudo(File.read('./test/ws-manual.pdf'))
-    ]
-
-    retorno_geracao_procedimento = Imasei::Servico.gerar_procedimento(
-                                      '110000949',
-                                      procedimento,
-                                      documentos,
-                                      procedimentos_relacionados = [],
-                                      unidades_envio = [],
-                                      manter_aberto_unidade = 'N',
-                                      enviar_email_notificacao = 'N',
-                                      data_retorno_programado = nil,
-                                      dias_retorno_programado = nil,
-                                      dias_uteis_retorno_programado = 'N')
-
-    refute_nil retorno_geracao_procedimento.id_procedimento
-  end
-
   it 'gera um procedimento com 2 documentos recebidos' do
-    procedimento = Imasei::Estruturas::Procedimento.new
-                    .id_tipo_procedimento('20')
+    procedimento = Wssei::Estruturas::Procedimento.new
+                    .id_tipo_procedimento('26')
                     .especificacao('Especificação do processo')
                     .assunto('01.01.01', 'Licitação')
                     .interessado('leandro.telles', 'Leandro Telles')
                     .observacao('Observação do webservice')
                     .nivel_de_acesso('0')
     documentos = [
-      Imasei::Estruturas::Documento.new
+      Wssei::Estruturas::Documento.new
         .tipo('R')
         .id_serie('20')
         .numero('Nome na árvore')
@@ -164,7 +29,7 @@ describe 'imasei' do
         .remetente('Remetente do documento')
         .nome_arquivo('ws-manual.pdf')
         .conteudo(File.read('./test/ws-manual.pdf')),
-      Imasei::Estruturas::Documento.new
+      Wssei::Estruturas::Documento.new
         .tipo('R')
         .id_serie('20')
         .numero('Nome na árvore')
@@ -174,8 +39,8 @@ describe 'imasei' do
         .conteudo(File.read('./test/logo-sei.png'))
     ]
 
-    retorno_geracao_procedimento = Imasei::Servico.gerar_procedimento(
-                                      '110000949',
+    retorno_geracao_procedimento = Wssei::Servico.gerar_procedimento(
+                                      '110001176',
                                       procedimento,
                                       documentos,
                                       procedimentos_relacionados = [],
@@ -189,42 +54,5 @@ describe 'imasei' do
     refute_nil retorno_geracao_procedimento.id_procedimento
   end
 
-  it 'gera um procedimento e adiciona 1 documento gerado' do
-    procedimento = Imasei::Estruturas::Procedimento.new
-                    .id_tipo_procedimento('20')
-                    .especificacao('Especificação do processo')
-                    .assunto('01.01.01', 'Licitação')
-                    .interessado('leandro.telles', 'Leandro Telles')
-                    .observacao('Observação do webservice')
-                    .nivel_de_acesso('0')
-    
-    retorno_geracao_procedimento = Imasei::Servico.gerar_procedimento(
-                                      '110000949',
-                                      procedimento,
-                                      documentos = [],
-                                      procedimentos_relacionados = [],
-                                      unidades_envio = [],
-                                      manter_aberto_unidade = 'N',
-                                      enviar_email_notificacao = 'N',
-                                      data_retorno_programado = nil,
-                                      dias_retorno_programado = nil,
-                                      dias_uteis_retorno_programado = 'N')
-
-    documento =
-      Imasei::Estruturas::Documento.new
-        .tipo('G')
-        .id_procedimento(retorno_geracao_procedimento.id_procedimento)
-        .id_serie('20')
-        .descricao('Descrição do documento 1')
-        .remetente('Remetente do documento 1')
-        .observacao('Observação do documento 1')
-        .conteudo('Conteúdo do documento 1')
-
-    retorno_inclusao_documento = Imasei::Servico.incluir_documento(
-                                    '110000949',
-                                    documento)
-
-    refute_nil retorno_inclusao_documento.id_documento
-  end
 
 end
