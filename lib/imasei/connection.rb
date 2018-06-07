6ec77528bf2f981132afeb5e6b06260de71fd853
Paulo Pinho <paulo.pinho@mabuya.tech>
2018-06-07 16:24:29 -0300

    Modificado tempo de request para 5 minutos.

--- lib/imasei/connection.rb
+++ lib/imasei/connection.rb
@@ -7,7 +7,7 @@ module Imasei
     include Singleton
 
     def client
-      @client ||= Savon.client wsdl: Imasei.configuration.wsdl, follow_redirects: Imasei.configuration.follow_redirects,
+      @client ||= Savon.client wsdl: Imasei.configuration.wsdl, follow_redirects: Imasei.configuration.follow_redirects, open_timeout: 300, read_timeout: 300,
                                 pretty_print_xml: Imasei.configuration.pretty_print_xml, convert_request_keys_to: :camelcase,
                                 encoding: Imasei.configuration.encoding, endpoint: Imasei.configuration.endpoint,
                                 ssl_verify_mode: Imasei.configuration.ssl_verify_mode
